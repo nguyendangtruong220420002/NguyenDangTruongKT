91a98dd5348706ed0bdc86aaab83560957fedd85
nguyendangtruong220420002 <nguyendangtruong22042002@gmail.com>
2023-11-17 10:33:07 +0700

    x nut them

--- App/App.js
+++ App/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Image, } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 
 const url = 'https://6544e42e5a0b4b04436d29bc.mockapi.io/Todo/todo1';
 export default function App() {
@@ -17,24 +17,65 @@ export default function App() {
   useEffect(() => {
     fetchData();
   }, []);
-  
+  const Post = () => {
+    fetch(url,
+      {
+        method: 'POST',
+        boby: JSON.stringify({
+          name: "Ronaldo",
+          img: "https://media.bongda.com.vn/files/thach.pham/2022/11/05/r-0819.jpg"
+        }),
+        headers: {
+          'Accept': 'application/json',
+          'Content-type': "application/json; charset:UTF-8",
+        },
+      })
+      .then((Res) => Res.json())
+      .then((json => {
+        setData([...data, json]);
+        console.log(json);
+      }))
+  };
+  const Delete = (id) => {
+    fetch(url + '/' + id,
+      // `${url}/${id}`
+      {
+        method: 'DELETE',
+      })
+      .then(() => {
+        // Lọc mục đã xóa khỏi trạng thái
+        setData(data.filter((item) => item.id !== id));
+      })
+      .catch((error) => console.error('Error deleting item:', error));
+  };
   return (
     <View style={styles.container}>
       {
         data
-          // .filter((item ) => item.id === '1') // Xuất mot minh id tất cả data 
-          .map((item) => {                     // xuat tat ca data
+          .map((item) => {                     
             console.log(item);
             return (
-              <View key={item.id}>
-                <Text style={{ fontSize: '20px' }}>
+              <View key={item.id} >
+                <Text style={{ fontSize: '20px' , color:'#000000', fontWeight:'bold' }}>
                   name :{item.name}
+                  </Text>
+                  <Text style={{ fontSize: '20px' , color:'#000000', fontWeight:'bold' }}>
                   id :{item.id}</Text>
                 <Image
                   source={item.img}
-                  style={{ width: '100px', height: '100px', resizeMode: 'contain', borderRadius: '200px', left: '100px' }}
+                  style={{ width: '100px', height: '100px', resizeMode: 'contain', borderRadius: '10px', left: '40px' }}
                 />
-                
+                <Pressable style={{ backgroundColor: "cyan", padding: '10px', borderRadius: '20px' }}
+                  onPress={() => { Post(item.id)
+                  }}
+                >
+                  <Text style={{ color: "black" }}>PUT</Text>
+                </Pressable>
+                <Pressable style={{ backgroundColor: "red", padding: '10px', borderRadius: '20px', marginTop: '10px' }}
+                  onPress={() => { Delete(item.id) }}
+                >
+                  <Text style={{ color: "white" }}>Delete</Text>
+                </Pressable>
               </View>
             )
           }
